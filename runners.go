ab1290c01e7a24756b44ccae8a888894a611ab92
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-08-16 13:26:49 +0200

    runners and Run now return an erro
    will return an error if something went wrong while trying to run the
    file

--- runners.go
+++ runners.go
@@ -12,26 +12,31 @@ type RunResult struct {
 	StdErr     io.Reader
 }
 
-type CodeRunner func(codeFile string) RunResult
+type CodeRunner func(codeFile string) (RunResult, error)
 
 func Run(codeFile string) (RunResult, error) {
 	progLang, err := GetProgLang(codeFile)
 	if err != nil {
 		return RunResult{}, err
 	}
-	return progLang.Runner(codeFile), nil
+	return progLang.Runner(codeFile)
 }
 
-func PythonRunner(codeFile string) RunResult {
+func PythonRunner(codeFile string) (RunResult, error) {
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
+
 	app := "python3"
 	command := exec.Command(app, codeFile)
 	command.Stdout = &stdout
 	command.Stderr = &stderr
 	rr := RunResult{}
-	_ = command.Run()
+	err := command.Run()
+	if err != nil {
+		return RunResult{}, err
+	}
+
 	rr.StdOut = &stdout
 	rr.StdErr = &stderr
-	return rr
+	return rr, nil
 }
--- runners_test.go
+++ runners_test.go
@@ -2,6 +2,7 @@ package Anubis
 
 import (
 	"io"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,7 +10,14 @@ import (
 
 func TestRunner(t *testing.T) {
 	t.Parallel()
-	rr, _ := Run("hello.py")
+	filename := "AnubisRunnerTestHello.py"
+	pyFile, err := os.Create(filename)
+	assert.Nil(t, err)
+	defer os.Remove(pyFile.Name())
+	_, err = pyFile.WriteString(`print("Hello World")`)
+	assert.Nil(t, err)
+
+	rr, _ := Run(filename)
 	out, _ := io.ReadAll(rr.StdOut)
 	assert.Equal(t, string(out), "Hello World\n", "Expcted: %d, Got: %d", "Hello World", string(out))
 }
