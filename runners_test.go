90c61692bc54056bc792e49b31eb95f1b4724582
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-08-16 22:06:13 +0200

    added test for LocalCommandRunner

--- runners_test.go
+++ runners_test.go
@@ -4,18 +4,39 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestRunner(t *testing.T) {
+func TestLocalCommandRunner(t *testing.T) {
+	t.Parallel()
+	fileName := "test.txt"
+	file, err := os.Create(fileName)
+	require.Nil(t, err)
+	file.WriteString("hello world")
+	file.Close()
+
+	f, err := os.Open(fileName)
+	require.Nil(t, err)
+
+	lcr := LocalCmdRunner{Input: f}
+	cmd := exec.Command("cat")
+	rr, err := lcr.RunCommand(cmd)
+	require.Nil(t, err)
+	out, err := io.ReadAll(rr.StdOut)
+	require.Nil(t, err)
+	require.Equal(t, "hello world", string(out))
+}
+
+func TestLocalRunner(t *testing.T) {
 	t.Parallel()
 	filename := "AnubisRunnerTestHello.py"
 	pyFile, err := os.Create(filename)
 	require.Nil(t, err)
 	_, err = pyFile.WriteString(`print(int(input())+20)`)
-	defer pyFile.Close()
+	pyFile.Close()
 	defer os.Remove(filename)
 	require.Nil(t, err)
 
@@ -23,7 +44,7 @@ func TestRunner(t *testing.T) {
 	inputFile, err := os.Create(inputFileName)
 	require.Nil(t, err)
 	_, err = inputFile.WriteString("400")
-	defer inputFile.Close()
+	inputFile.Close()
 	defer os.Remove(inputFileName)
 	require.Nil(t, err)
 
